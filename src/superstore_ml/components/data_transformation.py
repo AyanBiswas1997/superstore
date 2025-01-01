a12d3fe8afd65cdb36b4e77646f3941a450f2ae6
Ayan Biswas <137750296+AyanBiswas1997@users.noreply.github.com>
2025-01-01 13:33:56 +0530

    ok

--- src/superstore_ml/components/data_transformation.py
+++ src/superstore_ml/components/data_transformation.py
@@ -1,37 +1,43 @@
+
+import os
+import sys
 import pandas as pd
 import numpy as np
-import os 
-import sys
+
 from dataclasses import dataclass
-from src.superstore_ml.logger import logging
 from src.superstore_ml.exception import customexception
+from src.superstore_ml.logger import logging
 
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder,OrdinalEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
 from src.superstore_ml.utils.utils import save_object
-
-
-
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
 
 class DataTransformation:
     def __init__(self):
-        self.data_tranformation_config=DataTransformationConfig()
+        self.data_transformation_config=DataTransformationConfig()
 
-
+        
+    
     def get_data_transformation(self):
+        
         try:
-            logging.info("Data transformation started")
-            categorical_column=['Item_Fat_Content', 'Item_Type', 'Outlet_Size', 'Outlet_Location_Type',
+            logging.info('Data Transformation initiated')
+            
+            # Define which columns should be ordinal-encoded and which should be scaled
+            categorical_cols = ['Item_Fat_Content', 'Item_Type', 'Outlet_Size', 'Outlet_Location_Type',
        'Outlet_Type']
-            numerical_column=['Item_Weight', 'Item_Visibility', 'Item_MRP',
+            numerical_cols = ['Item_Weight', 'Item_Visibility', 'Item_MRP',
        'Outlet_Establishment_Year']
+            
+            # Define the custom ranking for each ordinal variable
             Item_Fat_Content_category=['Low Fat', 'Regular', 'low fat', 'LF', 'reg']
             Item_Type_category=['Dairy', 'Soft Drinks', 'Meat', 'Fruits and Vegetables', 'Household',
        'Baking Goods', 'Snack Foods', 'Frozen Foods', 'Breakfast',
@@ -42,88 +48,93 @@ class DataTransformation:
             Outlet_Type_category=['Supermarket Type1', 'Supermarket Type2', 'Grocery Store',
        'Supermarket Type3']
             
-        #pipeline started
-            logging.info("pipeline started")
-        #numeric pipeline
+            logging.info('Pipeline Initiated')
+            
+            ## Numerical Pipeline
             num_pipeline=Pipeline(
                 steps=[
-            ("imputer",SimpleImputer(strategy="most_frequent")),
-            ("scaler",StandardScaler())
-            ])
-        #categorical pipeline
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+            
+            # Categorigal Pipeline
             cat_pipeline=Pipeline(
                 steps=[
-            ("imputer",SimpleImputer(strategy="most_frequent")),
-            ("ordinal",OrdinalEncoder(categories=[Item_Fat_Content_category,Item_Type_category,Outlet_Size_category,Outlet_Location_Type_category,Outlet_Type_category]))
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('ordinalencoder',OrdinalEncoder(categories=[Item_Fat_Content_category,Item_Type_category,Outlet_Size_category,Outlet_Location_Type_category,Outlet_Type_category])),
+                ('scaler',StandardScaler())
+                ]
 
-            ]) 
-
-        #preprocessor 
-            preprocessor=ColumnTransformer(
-                steps=[
-            ("num",num_pipeline,numerical_column),
-            ("cat",cat_pipeline,categorical_column)
+            )
+            
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numerical_cols),
+            ('cat_pipeline',cat_pipeline,categorical_cols)
             ])
             
             return preprocessor
-
-
             
 
             
-
-
-
+            
+        
         except Exception as e:
-            logging.error(f"Error occured in data transformation {e}")
-            raise customexception(e,sys)
+            logging.info("Exception occured in the initiate_datatransformation")
 
-    def initiate_data_transformation(self,train_path,test_path):
+            raise customexception(e,sys)
+            
+    
+    def initialize_data_transformation(self,train_path,test_path):
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
-
-            logging.info("read train and test data")
-            logging.info(f"Train dataframe head: \n{train_df.head().to_string()}")
-            logging.info(f"Test dataframe head: \n{test_df.head().to_string()}")
-
-            preprocessor_obj=self.get_data_transformation()
-
-            target_column="Item_Outlet_Sales"
-            drop_columns=["Item_Identifier","target_column","Outlet_Identifier"]
-
-
-
-            input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
-            target_feature_train_df=train_df[target_column] 
-
+            
+            logging.info("read train and test data complete")
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head : \n{test_df.head().to_string()}')
+            
+            preprocessing_obj = self.get_data_transformation()
+            
+            target_column_name = 'Item_Outlet_Sales'
+            drop_columns = [target_column_name,'Item_Identifier','Outlet_Identifier']
+            
+            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            
+            
             input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
-            target_feature_test_df=test_df[target_column]
-
-            input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessor_obj.transform(input_feature_test_df)
-
+            target_feature_test_df=test_df[target_column_name]
+            
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            
             logging.info("Applying preprocessing object on training and testing datasets.")
-
+            
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             save_object(
-                file_path=self.data_tranformation_config.preprocessor_obj_file_path,
-                obj=preprocessor_obj()
-
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
             )
-            logging.info("Preprocessor object saved in artifacts folder")
+            
+            logging.info("preprocessing pickle file saved")
+            
             return (
                 train_arr,
                 test_arr
             )
-
-
-
+            
         except Exception as e:
-            logging.error(f"Error occured in data transformation {e}")
+            logging.info("Exception occured in the initiate_datatransformation")
+
             raise customexception(e,sys)
+            
+    
 
 
 
--- src/superstore_ml/pipelines/training_pipeline.py
+++ src/superstore_ml/pipelines/training_pipeline.py
@@ -8,25 +8,10 @@ from src.superstore_ml.exception import customexception
 from src.superstore_ml.components.model_trainer import ModelTrainer
 import pandas as pd
 obj=DataIngestion()
-obj.initiate_data_ingestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
 
+data_transformation=DataTransformation()
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
-def start_data_transformation(self,train_data_path,test_data_path):     
-    try:
-        transformation = DataTransformation()
-        train_arr,test_arr=transformation.initiate_data_transformation(train_data_path,test_data_path)
-        return train_arr,test_arr
-    except Exception as e:
-        raise customexception(e,sys)
-            
-            
-            
-        
-    
-def start_model_training(self,train_arr,test_arr):
-    try:
-        model_trainer=ModelTrainer()
-        model=model_trainer.initiate_model_training(train_arr,test_arr)
-        return model
-    except Exception as e:
-        raise customexception(e,sys)
+model_trainer=ModelTrainer()
+model_trainer.initiate_model_training(train_arr,test_arr)
